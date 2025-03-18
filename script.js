240bd73cf3b22105e1e1f1ce467c1260315998d6
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2025-03-18 22:19:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var isDate = function (input) {
   //   write your code here
-	if (input instanceof Date && !isNaN(input)) {
+	if (Object.prototype.toString.call(input) === '[object Date]' && !isNaN(input)) {
     return true;
   }
   
-  // Check if input is a valid date string
+  // Check if input is a valid date string or number
   if (typeof input === 'string' || typeof input === 'number') {
     const date = new Date(input);
     return !isNaN(date.getTime());
@@ -12,7 +12,6 @@ var isDate = function (input) {
   
   return false;
 };
-
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
